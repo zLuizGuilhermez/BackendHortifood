bc203659313a3c5fd84c38512099b9315636bdb7
Luiz Guilherme <luize.software@gmail.com>
2025-03-26 16:48:01 -0300

    primeira versão do projeto

---
+++ src/main/java/HortiFood/com/demo/HortiFoodApplication.java
@@ -0,0 +1,13 @@
+package HortiFood.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortiFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortiFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HortiFood/com/demo/HortiFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package HortiFood.com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortiFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
